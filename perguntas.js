32b7aa391fb3a7bff3350e1d0f184b561d96c6c5
robsonmac <robsonmac3@gmail.com>
2024-11-13 20:42:30 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    'O que é Python?',
+    'O Python é uma linguagem de programação'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital do Ceará?',
+    'A capital da Ceará é Fortaleza'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz bom dia em Inglês?',
+    'Bom dia em ingles é good morning (RAI)'
+)
